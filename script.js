4899632daf725287fcdd32ae054aaea6b8ea198c
Julia W <w.julia91@gmail.com>
2020-05-31 13:59:55 +0200

    added vueJS

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello Snappet!");
+var app = new Vue({
+    el: '#app',
+    data: {
+        
+    }
+})
+
+
+
+
+
+
+
+
+/*
+What does the app do?
+- If dropdown selected data will change on the UI.
+- Event listener for dropdown.
+- DOM manipulation.
+
+1. If different dropdown selected, console.log out sg.
+    - Default selected.
+    - Which one is selected?
+    - Need to get the value.
+    - Need to store the value.
+2. If different dropdown selected, change the UI w DOM manipulation.
+3. Structure and shorten the code.
+    - global controller
+    - collect var-s
+
+*/
