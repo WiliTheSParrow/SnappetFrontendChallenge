4fd6b522aa01078591e3df1e3b32846cbdc766fa
Julia W <w.julia91@gmail.com>
2020-05-31 19:03:08 +0200

    finished

--- script.js
+++ script.js
@@ -2,23 +2,41 @@ var app = new Vue({
     el: '#app',
     data: {
         selected: '',
-
+        pupilArray: [],
+        topGradePupil: {
+            divClass: 'content__pupils--pupil topgrade'
+        },
+        averageGradePupil: {
+            divClass: 'content__pupils--pupil average'
+        },
+        failedPupil: {
+            divClass: 'content__pupils--pupil failed'
+        },
     },
-
+    methods: {
+        fillPupilArray: function (numberOfPupils, pupilType) {
+            for (var i = 0; i < numberOfPupils; i++) {
+                this.pupilArray.push(pupilType);
+            }
+        },
+        completePupilArray: function (numberOfTopGrades, numberOfAverageGrades, numberOfFailed) {
+            app.fillPupilArray(numberOfTopGrades, this.topGradePupil);
+            app.fillPupilArray(numberOfAverageGrades, this.averageGradePupil);
+            app.fillPupilArray(numberOfFailed, this.failedPupil);
+        },
+        initiateFill: function (selection) {
+            this.pupilArray = [];
+            switch (selection) {
+                case 'grade3':
+                    app.completePupilArray(16, 24, 0);
+                    break;
+                case 'grade4':
+                    app.completePupilArray(3, 34, 3);
+                    break;
+                case 'grade5':
+                    app.completePupilArray(3, 34, 3);
+                    break;
+            }
+        }
+    }
 })
-
-
-/*
-id='mathGrade-3'
-id='mathGrade-4'
-id='mathGrade-5'
-
-
-
-
-<div class="content__pupils--pupil pupil-1 topgrade"></div>
-<div class="content__pupils--pupil pupil-4 average"></div>
-<div class="content__pupils--pupil pupil-40 failed"></div>
-
-
-*/
